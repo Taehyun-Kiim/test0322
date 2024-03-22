e10ed3ca8d03b1971ab1b8e851e17faf4a935c15
Taehyun-Kiim <162668891+Taehyun-Kiim@users.noreply.github.com>
2024-03-22 15:45:24 +0900

    update

--- main.js
+++ main.js
@@ -24,3 +24,57 @@ const circles = svg
   .attr("r", (d) => d.radius)
   .attr("fill", "blue")
   .attr("stroke", "black");
+
+const data = [
+  {
+    fruit: "사과",
+    quantity: 20,
+    details: {
+      color: "red",
+      price: 1.2,
+    },
+  },
+  {
+    fruit: "바나나",
+    quantity: 40,
+    details: {
+      color: "yellow",
+      price: 0.8,
+    },
+  },
+  {
+    fruit: "체리",
+    quantity: 60,
+    details: {
+      color: "red",
+      price: 2.5,
+    },
+  },
+  {
+    fruit: "딸기",
+    quantity: 80,
+    details: {
+      color: "red",
+      price: 1.8,
+    },
+  },
+  {
+    fruit: "포도",
+    quantity: 100,
+    details: {
+      color: "purple",
+      price: 2.0,
+    },
+  },
+];
+
+const circles = svg
+  .selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr("cx", (d) => d.x)
+  .attr("cy", (d) => d.y)
+  .attr("r", (d) => d.radius)
+  .attr("fill", "blue")
+  .attr("stroke", "black");
